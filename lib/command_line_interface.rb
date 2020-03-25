1d3b7167d4cdc5a3807b693ce8f7ac8ccb80e9ea
github username <github email address>
2020-03-25 15:50:32 -0700

    started working on differnt choices users can make using the CLI

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -19,23 +19,105 @@ def welcome
     puts "　    ﾉﾍ　　　ヽ                           l"
 end
 
-def choices
+def main_menu
     puts "Please input a number for the option you would like to choose:"
-    puts "1. List all Ghibli film titles"
-    puts "2. List my favorites"
-    puts "3. Add a film to my favorites by name"
-    puts "0. Exit the CLI"
+    puts "1. Learn about Ghibli films"
+    puts "2. Learn about Ghibli film locations"
+    puts "3. Learn about Ghibli film characters"
+    puts "4. Learn about Ghibli character species"
+    puts "5. Learn a random fact about Ghibli films"
+    puts "0. Exit the application"
     gets.strip
 end
 
-def film_list
+def choices
+    choice = main_menu
+    if choice == "0"
+    elsif choice == "1"
+        film_choice
+    elsif choice == "2"
+        location_choice
+    elsif choice == "3"
+        character_choice
+    elsif choice == "4"
+        species_choice
+    elsif choice == "5"
+        random_fact
+    else
+        puts "That input was invalid. Please try again"
+        choices
+    end
+end
+
+def film_choice
+    puts "Here is a list of all Ghibli film titles"
     Film.all.each { |film| puts film.title }
+    puts "Input a film from the list you would like to learn about:"
+    film = gets.strip
+    print_film(film)
+    film_menu(film)
+end
+
+def print_film(film)
+end
+
+def film_menu(film)
+end
+
+def location_choice
+    puts "Here is a list of all Ghibli film location names"
+    Location.all.each { |location| puts location.name }
+    puts "Input a location you would like to learn about:"
+    location = gets.strip
+    print_location(location)
+    location_menu(location)
+end
+
+def print_location(location)
+end
+
+def location_menu(location)
+    puts "Input a number for the option you would like to choose:"
+    puts ""
+end
+
+def character_choice
+    puts "Here is a list of all Ghibli film character names"
+    Character.all.each { |character| puts character.name }
+    puts "Input a character from the list you would like to learn about:"
+    character = gets.strip
+    print_character(character)
+    character_menu(character)
+end
+
+def print_character(character)
+end
+
+def character_menu(character)
+end
+
+def species_choice
+    puts "Here is a list of all Ghibli character species"
+    Species.all.each { |species| puts species.name }
+    puts "Input a species from the list you would like to learn about:"
+    species = gets.strip
+    print_species(species)
+    species_menu(species)
+end
+
+def print_species(species)
+end
+
+def species_menu(species)
+end
+
+def random_fact
+    facts = ["", "", "", "", ""]
 end
 
 def run
     welcome
-    choice = choices
-    film_list
+    choices
 end
 
 run
