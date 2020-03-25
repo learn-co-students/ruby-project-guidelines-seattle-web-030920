16b8e5ac93cb0dde22b1acb5781d2dcdd69d7557
github username <github email address>
2020-03-24 17:35:58 -0700

    finished the first CRUD method

--- lib/command_line_interface.rb
+++ lib/command_line_interface.rb
@@ -1,3 +1,5 @@
+require_relative '../config/environment.rb'
+
 def welcome
     puts "Welcome to the world of Studio Ghibli!"
     puts "　　　　　　　　　　　　　　　　 ﾍ"
@@ -19,10 +21,21 @@ end
 
 def choices
     puts "Please input a number for the option you would like to choose:"
-    puts "1. List all Ghibli films"
+    puts "1. List all Ghibli film titles"
     puts "2. List my favorites"
     puts "3. Add a film to my favorites by name"
     puts "0. Exit the CLI"
+    gets.strip
 end
 
-welcome
+def film_list
+    Film.all.each { |film| puts film.title }
+end
+
+def run
+    welcome
+    choice = choices
+    film_list
+end
+
+run
