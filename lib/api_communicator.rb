38e4076ce7c57370f2d226adc301262fe404fbe3
github username <github email address>
2020-03-26 17:10:16 -0700

    submit

---
+++ lib/api_communicator.rb
@@ -0,0 +1,12 @@
+# require 'rest-client'
+# require 'json'
+# require 'pry'
+
+  #make the web request
+  # res = RestClient.get('https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins=Washington,DC&destinations=New+York+City,NY&key=AIzaSyBcx5a5Ycmzs5rW4oLokzJ08FUcMh4COtE')
+  # binding.pry
+  #res = JSON.parse(res.body)
+
+  
+
+  
