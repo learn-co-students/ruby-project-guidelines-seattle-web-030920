f1abf711719d209b1dc03f86428e358d0267e8ca
fosterv2 <fosterv2@outlook.com>
2020-07-01 15:57:49 -0700

    added a file with my name in ASCII code

---
+++ bin/background.rb
@@ -0,0 +1,138 @@
+require 'colorize'
+
+# on default (black) background
+puts ""
+puts ""
+print "        .--.     /)                          ".light_cyan
+puts ".---.".light_cyan
+print "       /.''\\\\._.'/   ".light_cyan
+print ".----.     .----.       ".blue
+print "|   |      ".light_cyan
+print "__.....__             ".blue
+print ".--.      "
+puts "__.....__".blue
+print "      (/    `---'     ".light_cyan
+print "\\    \\   /    /        ".blue
+print "|   |  ".light_cyan
+print ".-''         '.           ".blue
+print "|__|  "
+puts ".-''         '.".blue
+print "                       '   '. /'   /         ".blue
+print "|   | ".light_cyan
+print "/     .-''\"'-.  `. ".blue
+print ".-,.--. ".light_cyan
+print ".--. "
+puts "/     .-''\"'-.  `.".blue
+print "                       |    |'    /    ".blue
+print "__    "
+print "|   |".light_cyan
+print "/     /________\\   \\".blue
+print "|  .-. |".light_cyan
+print "|  |"
+puts "/     /________\\   \\".blue
+print "                       |    ||    | ".blue
+print ".:--.'.  "
+print "|   |".light_cyan
+print "|                  |".blue
+print "| |  | |".light_cyan
+print "|  |"
+puts "|                  |".blue
+print "                       '.   `'   .'".blue
+print "/ |   \\ | "
+print "|   |".light_cyan
+print "\\    .-------------'".blue
+print "| |  | |".light_cyan
+print "|  |"
+puts "\\    .-------------'".blue
+print "                        \\        / ".blue
+print "`\" __ | | "
+print "|   | ".light_cyan
+print "\\    '-.____...---.".blue
+print "| |  '- ".light_cyan
+print "|  | "
+puts "\\    '-.____...---.".blue
+print "                         \\      /   ".blue
+print ".'.''| | "
+print "|   |  ".light_cyan
+print "`.             .' ".blue
+print "| |     ".light_cyan
+print "|__|  "
+puts "`.             .'".blue
+print "                          '----'   ".blue
+print "/ /   | |_"
+print "'---'    ".light_cyan
+print "`''-...... -'   ".blue
+print "| |             ".light_cyan
+puts "`''-...... -'".blue
+print "                                   \\ \\._,\\ '/                         "
+puts "|_|".light_cyan
+puts "                                    `--'  `\""
+puts ""
+puts ""
+
+# On white background
+# puts "                                                                                          ".on_white
+# puts "                                                                                          ".on_white
+# puts "                        .---.                                                             ".light_cyan.on_white
+# print ".----.     .----.       ".blue.on_white
+# print "|   |      ".light_cyan.on_white
+# print "__.....__             ".blue.on_white
+# print ".--.      ".black.on_white
+# puts "__.....__               ".blue.on_white
+# print " \\    \\   /    /        ".blue.on_white
+# print "|   |  ".light_cyan.on_white
+# print ".-''         '.           ".blue.on_white
+# print "|__|  ".black.on_white
+# puts ".-''         '.                ".blue.on_white
+# print "  '   '. /'   /         ".blue.on_white
+# print "|   | ".light_cyan.on_white
+# print "/     .-''\"'-.  `. ".blue.on_white
+# print ".-,.--. ".light_cyan.on_white
+# print ".--. ".black.on_white
+# puts "/     .-''\"'-.  `.             ".blue.on_white
+# print "  |    |'    /    ".blue.on_white
+# print "__    ".black.on_white
+# print "|   |".light_cyan.on_white
+# print "/     /________\\   \\".blue.on_white
+# print "|  .-. |".light_cyan.on_white
+# print "|  |".black.on_white
+# puts "/     /________\\   \\    ".blue.on_white
+# print "  |    ||    | ".blue.on_white
+# print ".:--.'.  ".black.on_white
+# print "|   |".light_cyan.on_white
+# print "|                  |".blue.on_white
+# print "| |  | |".light_cyan.on_white
+# print "|  |".black.on_white
+# puts "|                  |    ".blue.on_white
+# print "  '.   `'   .'".blue.on_white
+# print "/ |   \\ | ".black.on_white
+# print "|   |".light_cyan.on_white
+# print "\\    .-------------'".blue.on_white
+# print "| |  | |".light_cyan.on_white
+# print "|  |".black.on_white
+# puts "\\    .-------------'    ".blue.on_white
+# print "   \\        / ".blue.on_white
+# print "`\" __ | | ".black.on_white
+# print "|   | ".light_cyan.on_white
+# print "\\    '-.____...---.".blue.on_white
+# print "| |  '- ".light_cyan.on_white
+# print "|  | ".black.on_white
+# puts "\\    '-.____...---.        ".blue.on_white
+# print "    \\      /   ".blue.on_white
+# print ".'.''| | ".black.on_white
+# print "|   |  ".light_cyan.on_white
+# print "`.             .' ".blue.on_white
+# print "| |     ".light_cyan.on_white
+# print "|__|  ".black.on_white
+# puts "`.             .'        ".blue.on_white
+# print "     '----'   ".blue.on_white
+# print "/ /   | |_".black.on_white
+# print "'---'    ".light_cyan.on_white
+# print "`''-...... -'   ".blue.on_white
+# print "| |             ".light_cyan.on_white
+# puts "`''-...... -'        ".blue.on_white
+# print "              \\ \\._,\\ '/                         ".black.on_white
+# puts "|_|                                   ".light_cyan.on_white
+# puts "               `--'  `\"                                                                        ".black.on_white
+# puts "                                                                                                ".on_white
+# puts "                                                                                                ".on_white
